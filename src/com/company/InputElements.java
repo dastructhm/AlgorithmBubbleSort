ebe84c7c5a2cd4ba64de747c7504966233920723
bjdhjy888 <bjdhjy678@gmail.com>
2020-11-05 10:51:55 +0800

    updated

--- src/com/company/InputElements.java
+++ src/com/company/InputElements.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 
 public class InputElements {
     void SetElements(int arrayLength, int[] myArray){
-        // int[] myArray = {1, 3, 4, 6, 7, 8, 10, 13, 14, 18, 19, 21, 24, 37, 40, 45, 71};
         for(int i = 0; i < arrayLength; i++) {
             System.out.print("Enter element " + i + ": ");
             Scanner UserInput = new Scanner(System.in);
